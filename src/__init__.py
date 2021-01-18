3cddf6a8d957eb7ae21d52f68d6abe629be0308f
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-01-18 17:31:23 -0500

    Bill Wurtz just uploaded I feel motivated to do ANYTHING

--- src/__init__.py
+++ src/__init__.py
@@ -9,6 +9,12 @@ https://www.kite.com/python/answers/how-to-read-a-dictionary-from-a-file-in--pyt
 https://stackoverflow.com/a/7689085/9654083
 """
 
+def APILoad():
+    print("Loading API...")
+    loaded = API.api()
+    api = API.parse(loaded)
+    return api
+
 if sys.argv == []:
     print(datas.usage)
     sys.exit(1)
@@ -21,19 +27,13 @@ elif "--help" in sys.argv:
 elif "-v" in sys.argv or "--version" in sys.argv: 
     print(datas.version)
 elif sys.argv[1] == "install": 
-    print("Loading API...")
-    loaded = API.api()
-    api = API.parse(loaded)
+    api = APILoad()
     sys.exit(127) #mimics the shell command not found error code
 elif sys.argv[1] == "remove":
-    print("Loading API...")
-    loaded = API.api()
-    api = API.parse(loaded)
+    api = APILoad()
     sys.exit(127)
 elif sys.argv[1] == "list" or sys.argv[1] == "show":
-    print("Loading API...")
-    loaded = API.api()
-    api = API.parse(loaded)
+    api = APILoad()
     sys.exit(127)
 elif "-v" in sys.argv or "--version" in sys.argv: 
     print(datas.version)
