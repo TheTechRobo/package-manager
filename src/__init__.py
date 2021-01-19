950a3b4afc3ed29e0930034e5015c073b8d8cef0
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-01-18 19:28:20 -0500

    WIP

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,13 @@
 import CONFIG_HANDLER, sys
 import DATA as datas
 import API
+from io import BytesIO #https://stackoverflow.com/a/5711095/9654083
+from zipfile import ZipFile
+import requests
+import os
+
+#CONSTANTS
+BASE_PATH = "~/.local/bin/thetechrobopackagemanager"
 
 """
 credits
@@ -12,7 +19,7 @@ https://stackoverflow.com/a/7689085/9654083
 APILoad = API.APILoad
 
 try:
-    with open("%s/initialised","r") as file:
+    with open("%s/initialised" % BASE_PATH,"r") as file:
         pass
 except Exception:
     print("TheTechRobo's packaged manager doesn't seem to be initialised. Initialise now? (Y/n)")
@@ -22,7 +29,7 @@ except Exception:
         sys.exit(127)
     else:
         sys.exit(3)
-
+packName = sys.argv[2]
 if sys.argv == []:
     print(datas.usage)
     sys.exit(1)
@@ -36,6 +43,16 @@ elif "-v" in sys.argv or "--version" in sys.argv:
     print(datas.version)
 elif sys.argv[1] == "install": 
     api = APILoad()
+    try:
+        pUrl = api[packName[url]]
+    except KeyError:
+        print("Could not find the requested package")
+        sys.exit(4)
+    res = requests.get(pUrl)
+    if res.status_code == 200: 
+        with open("%s/tmp/TEMP.zip", "wb") as file:
+            file.write(res.content)
+        
     sys.exit(127) #mimics the shell command not found error code
 elif sys.argv[1] == "remove":
     api = APILoad()
